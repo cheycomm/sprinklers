e2d7df65f7c707f5e0349e6276d03e309e297131
cheycomm <todd@nalamail.comm>
2016-11-01 14:57:34 -0400

    starting daily schedule

---
+++ sandbox/daily_schedule.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import time, sched
+
+fmt = '%m/%d/%y %H:%M:%S'
+fmt_time = time.strftime(fmt)
+sched_time_str = '11/01/16 14:18:00'
+sched_time = time.mktime(time.strptime(sched_time_str, fmt))
+tdelta = int(sched_time - time.time()) + 1
+scheduler = sched.scheduler(time.time, time.sleep)
+
+def print_event(name="default"):
+  print('EVENT', time.strftime(fmt), name)
+
+print( "Formatted time :", fmt_time)
+print("Unformatted time :", time.time())
+print("10/29/16 10:40:00 :", sched_time)
+print("Delta :", tdelta)
+
+print_event('Start')
+scheduler.enter(tdelta, 1, print_event)
+
+scheduler.run()
+print_event('Complete')
